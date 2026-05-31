e1981e02ca6c214ce0b79ee673a611311c363702
Tanya Tripathi <tanya.24bai10414@vitbhopal.ac.in>
2026-05-31 16:02:28 +0530

    Delete src directory

--- src/pages/Success.jsx
+++
@@ -1,8 +0,0 @@
-export default function Success() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>🎉 Account created successfully!</h1>
-      <p>Welcome to the app.</p>
-    </div>
-  );
-}
